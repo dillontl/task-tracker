0d9defc46488a3ce44abe0d79e116e70100af7a0
dillontl <dillon@lueraws.com>
2021-09-09 20:29:16 -0700

    first commit

---
+++ build/precache-manifest.1f98044ae39e2a487773cfafb6fb5e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f986938d3e95ba6cfe490558eed5474",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "64fe1ddc9f84c9e2bcd3",
+    "url": "/task-tracker/static/css/main.b30e35bf.chunk.css"
+  },
+  {
+    "revision": "700620c92771f724c4aa",
+    "url": "/task-tracker/static/js/2.7e52d439.chunk.js"
+  },
+  {
+    "revision": "ec19a7404159eb2ec065d16f9bab6f00",
+    "url": "/task-tracker/static/js/2.7e52d439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fe1ddc9f84c9e2bcd3",
+    "url": "/task-tracker/static/js/main.4ef54e2f.chunk.js"
+  },
+  {
+    "revision": "44e2746e3f7572562cbe",
+    "url": "/task-tracker/static/js/runtime-main.290efcd8.js"
+  }
+]);
